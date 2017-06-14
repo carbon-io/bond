49c7074513c53e00dbf1017294366e7e4e2ccfbd
Greg Banks <gbanks@gmail.com>
2017-06-13 18:18:04 -0700

    catch ES version mismatch errors
    this still needs work since the syntax-error module doesn't seem to
    catch these errors. in the meantime, we're just omitting the relevant
    information and not erroring again in bond itself.

---
+++ test/fixtures/esVersionMismatch.js
@@ -0,0 +1,5 @@
+class Foo {
+  constructor() {
+    this.name = 'foo'
+  }
+}
--- test/resolveModuleTests.js
+++ test/resolveModuleTests.js
@@ -1,11 +1,15 @@
 var assert = require('assert')
 
+var sinon = require('sinon')
+
 var __ = require('@carbon-io/fibers').__(module)
 var o = require('@carbon-io/atom').o(module)
 var testtube = require('@carbon-io/test-tube')
 
 var _o = require('..')._o(module)
 var errors = require('../lib/errors')
+
+NODE_VERSION = parseInt(process.version.match(/v(\d+)\.\d+\.\d+/)[1])
 
 __(function() {
   module.exports = o.main({
@@ -66,6 +70,29 @@ __(function() {
             assert.equal(err.columnNumber, 8)
             assert(err.message.match(/.+\/fixtures\/syntaxError\.js:4/) !== null)
             assert(err.stack.match(/.+\/fixtures\/syntaxError\.js:4/) !== null)
+            return true
+          })
+        }
+      }),
+      o({
+        _type: testtube.Test,
+        name: 'ResolveModuleWithUnsupportedLanguageConstructs',
+        description: 'Resolve a module with language features that are not supported in this version of node',
+        setup: function() {
+          if (NODE_VERSION > 4) {
+            throw new testtube.errors.SkipTestError('Only run in node 4')
+          }
+        },
+        doTest: function() {
+          assert.throws(function() {
+            var mod = _o('./fixtures/esVersionMismatch')
+          }, function(err) {
+            assert(err.error instanceof SyntaxError)
+            assert(err instanceof SyntaxError)
+            assert(err instanceof errors.ResolveModuleSyntaxError)
+            assert.equal(err.lineNumber, -1)
+            assert.equal(err.columnNumber, -1)
+            assert(typeof err.ecmaVersion === 'undefined')
             return true
           })
         }
